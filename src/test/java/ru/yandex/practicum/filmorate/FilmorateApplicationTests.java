32d570ef6abfddf9048c3e712182b411a00be183
Konstantin Stoyakin <gavrilbatya@yandex.ru>
2025-03-23 22:25:26 +0300

    Добавлены модели Film и User, реализованы контроллеры для пользователей и фильмов, добавлена базовая валидация данных и обработка ошибок, настроено логирование

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,13 +1,41 @@
 package ru.yandex.practicum.filmorate;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @SpringBootTest
 class FilmorateApplicationTests {
+	private Validator validator;
+
+	@BeforeEach
+	void setup() {
+		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+		validator = factory.getValidator();
+	}
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void shouldFailWhenEmailIsBlank() {
+		User user = new User();
+		user.setEmail("");
+		user.setLogin("privet-zemlya");
+		user.setBirthday(LocalDate.of(2000, 1, 1));
+
+		Set<ConstraintViolation<User>> violations = validator.validate(user);
+		assertFalse(violations.isEmpty());
+	}
 }
