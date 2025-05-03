9e34861fc65e6d7de1129569b618efc9067ddbf4
Konstantin Stoyakin <gavrilbatya@yandex.ru>
2025-05-03 21:18:16 +0300

    Добавлены зависимости для H2, конфигурация БД и имплементации DAO

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -14,7 +14,6 @@ import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 import java.time.LocalDate;
 import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
